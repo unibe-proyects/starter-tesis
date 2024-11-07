522ef2c2d36f4eff26b74c147a3ce8249a3c97e6
steveen ordoñez <zh311505@gmail.com>
2024-11-07 12:28:32 -0500

    feat: add const hola

--- src/pages/modules/home/HomeScreen.tsx
+++ src/pages/modules/home/HomeScreen.tsx
@@ -2,6 +2,7 @@ const HomeScreen = () => {
   const hola = '';
   const holax = '';
   const holau = '';
+  const holtu = '';
   return (
     <div className="bg-light" data-testid="home-screen">
       <button className="btn btn-neutral">hola</button>
