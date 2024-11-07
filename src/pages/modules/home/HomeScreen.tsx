e6bd32ebb6c8ee44f154de1f3f8aac6488a21c13
steveen ordoñez <zh311505@gmail.com>
2024-11-07 12:41:27 -0500

    feat: add const hola

--- src/pages/modules/home/HomeScreen.tsx
+++ src/pages/modules/home/HomeScreen.tsx
@@ -1,9 +1,9 @@
 const HomeScreen = () => {
-  const hola = 'sss';
-  const holax = 'ss';
-  const holau = 'sss';
-  const holtu = 'sss';
-  const holt = 'sss';
+  const hola = 'sswwwwws';
+  const holax = 'swws';
+  const holau = 'sssww';
+  const holtu = 'swwwss';
+  const holt = 'swwwss';
   return (
     <div className="bg-light" data-testid="home-screen">
       <button className="btn btn-neutral">hola</button>
